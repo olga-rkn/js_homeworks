a442f54b6bda8e1fdad1955cf285e3bc3f87070f
olga-rkn <292868296+olga-rkn@users.noreply.github.com>
2026-07-20 07:59:22 +0200

    Update js_hw7_2.js
    pattern option added

--- js_hw7_2.js
+++ js_hw7_2.js
@@ -18,6 +18,7 @@ var arr = [
 
 // option 1
 const pattern = /^[a-zA-Z0-9]+\.[a-zA-Z0-9]+@\byahoo\b|\bgmail\b\.\bcom\b$/;
+//const pattern = /^[a-zA-Z0–9._%+-]+@\byahoo\b|\bgmail\b\.\bcom\b$/;
 
 function email_checker(data) {
     for (element of data) {
